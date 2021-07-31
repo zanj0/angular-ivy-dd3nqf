3c3830e273ab88c59cb764af6f7495ca3ec5335b
zanj0 <agnibhachakraborty@gmail.com>
2021-07-31 21:43:31 +0530

    Data binding.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,27 @@ import { Component } from '@angular/core';
   // selector: 'app-server',
   // selector: '[app-server]',
   selector: '.app-server',
-  // templateUrl: './server.component.html'
-  template: '<h3> Hello World!</h3>'
+  templateUrl: './server.component.html'
+  // template: '<h3> Hello World!</h3>'
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus = 'offline';
+  allowNewServer = false;
+  serverCreationStatus = "No server was created.";
+  serverName = 'Test Server';
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
+  getServerStatus(){
+    return this.serverStatus;
+  }
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created. Name is " + this.serverName;
+  }
+  onUpdateServerName(event: any){
+    this.serverName = event.target.value;
+  }
+}
