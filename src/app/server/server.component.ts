48d602355dddd63274e6d6cd69796bd35c9d5c01
zanj0 <agnibhachakraborty@gmail.com>
2021-07-31 22:43:28 +0530

    Directive implementation.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,28 +2,41 @@ import { Component } from '@angular/core';
 @Component({
   // selector: 'app-server',
   // selector: '[app-server]',
-  selector: '.app-server',
-  templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles:[`
+    .online{
+      color: white;
+    }
+  `]
   // template: '<h3> Hello World!</h3>'
 })
 export class ServerComponent {
   serverId: number = 10;
   serverStatus = 'offline';
   allowNewServer = false;
-  serverCreationStatus = "No server was created.";
+  serverCreationStatus = 'No server was created.';
   serverName = 'Test Server';
-  constructor(){
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    },2000);
+    }, 2000);
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
   }
-  getServerStatus(){
+  getServerStatus() {
     return this.serverStatus;
   }
-  onCreateServer(){
-    this.serverCreationStatus = "Server was created. Name is " + this.serverName;
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus ='Server was created. Name is ' + this.serverName;
+    this.servers.push(this.serverName);
   }
-  onUpdateServerName(event: any){
+  onUpdateServerName(event: any) {
     this.serverName = event.target.value;
+  }
+  getColor() {
+    return this.serverStatus == 'online' ? 'green' : 'red';
   }
 }
